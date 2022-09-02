cfa9ee834c75d864e5680bea90ed56c5716c73c2
DTGuy <tguy5837@gmail.com>
2022-09-02 16:58:22 -0400

    added uv index functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-var currentWeatherContainer = document.querySelector(".current-weather");
+var currentWeatherContainer = document.querySelector(".weather-results");
 
 var getWeatherData = function (cityName) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=cbf23c1f1f5aaf3179f3e715be9b2e92";
@@ -7,8 +7,15 @@ var getWeatherData = function (cityName) {
     fetch(apiUrl).then(function (response) {
         // response was successful
         if (response.ok) {
-            response.json().then(function (data) {
-                displayWeatherData(cityName, data);
+            response.json().then(function (weatherData) {
+                // store lat & lon in variables for uv search
+                var latitude = weatherData.coord.lat;
+                console.log(latitude);
+                var longitude = weatherData.coord.lon;
+                console.log(longitude);
+
+                // get uv index from uv api
+                getUVIndex(latitude, longitude, weatherData);
             });
         } else {
             alert("Problem getting weather data!");
@@ -16,7 +23,7 @@ var getWeatherData = function (cityName) {
     });
 };
 
-var getUVIndex = function (latitude, longitude) {
+var getUVIndex = function (latitude, longitude, weatherData) {
     // get lon & lat from data
     var apiUrl = "http://api.openweathermap.org/data/2.5/uvi?appid=cbf23c1f1f5aaf3179f3e715be9b2e92&amp;lat=" + latitude + "&amp;lon=" + longitude;
 
@@ -24,7 +31,7 @@ var getUVIndex = function (latitude, longitude) {
         // response was successful
         if (response.ok) {
             response.json().then(function (uvData) {
-                console.log(uvData.value);
+                displayWeatherData(weatherData, uvData);
             });
         } else {
             alert("Problem getting UV Index!")
@@ -33,48 +40,40 @@ var getUVIndex = function (latitude, longitude) {
 
 };
 
-var displayWeatherData = function (cityName, data) {
-    console.log(data);
-    console.log(data.name);
-    console.log(data.main.temp + " Degrees");
-    console.log(data.wind.speed + " MPH");
-    console.log(data.weather[0].icon);
+var displayWeatherData = function (weatherData, uvData) {
+    console.log(weatherData);
+    console.log(weatherData.name);
+    console.log(weatherData.main.temp + " Degrees");
+    console.log(weatherData.wind.speed + " MPH");
+    console.log(weatherData.weather[0].icon);
 
     // create current weather card
     var currentWeatherEl = document.createElement("card");
-    currentWeatherEl.classList = "m-2";
+    currentWeatherEl.classList = "d-flex flex-column border border-dark p-2 m-2 w-100";
 
     // create h element for current weather title
     var cityDateEl = document.createElement("h3")
-    cityDateEl.textContent = data.name + " (Add moment.js to get date here!)";
+    cityDateEl.textContent = weatherData.name + " (" + moment().format("MM/DD/YYYY") + ")";
     currentWeatherEl.appendChild(cityDateEl);
 
     // create p element for current temp
     var tempEl = document.createElement("p");
-    tempEl.textContent = data.main.temp + " Degrees";
+    tempEl.textContent = "Temp: " + weatherData.main.temp + " Degrees";
     currentWeatherEl.appendChild(tempEl);
 
     // create p element for current wind speed
     var windEl = document.createElement("p");
-    windEl.textContent = data.wind.speed + " MPH";
+    windEl.textContent = "Wind: " + weatherData.wind.speed + " MPH";
     currentWeatherEl.appendChild(windEl);
 
     // create p element for current humidity
     var humidityEl = document.createElement("p");
-    humidityEl.textContent = data.main.humidity + "%";
+    humidityEl.textContent = "Humidity: " + weatherData.main.humidity + "%";
     currentWeatherEl.appendChild(humidityEl);
-
-    // Get uv index from open weather uv api:
-    //  a. Store lat and lon into variables to search uv api
-    var latitude = data.coord.lat;
-    console.log(latitude);
-    var longitude = data.coord.lon;
-    console.log(longitude);
 
     // create p element for UV Index
     var uvEl = document.createElement("p");
-    // get uv index from lat & lon
-    uvEl.textContent = getUVIndex(latitude, longitude);
+    uvEl.textContent = "UV Index: " + uvData.value;
     currentWeatherEl.appendChild(uvEl);
 
     // append currentWeatherEl to parent container on screen
