350f71856b194051a4dbf762d632d538e38b2e9e
DTGuy <tguy5837@gmail.com>
2022-09-02 20:08:26 -0400

    added forecast weather

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,6 @@
-var currentWeatherContainer = document.querySelector(".weather-results");
+var currentWeatherContainer = document.querySelector(".current-weather");
+
+var forecastWeatherContainer = document.querySelector(".forecast-weather");
 
 var getWeatherData = function (cityName) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=cbf23c1f1f5aaf3179f3e715be9b2e92";
@@ -15,7 +17,7 @@ var getWeatherData = function (cityName) {
                 console.log(longitude);
 
                 // get uv index from uv api
-                getUVIndex(latitude, longitude, weatherData);
+                getUVIndex(weatherData, latitude, longitude);
             });
         } else {
             alert("Problem getting weather data!");
@@ -23,7 +25,7 @@ var getWeatherData = function (cityName) {
     });
 };
 
-var getUVIndex = function (latitude, longitude, weatherData) {
+var getUVIndex = function (weatherData, latitude, longitude) {
     // get lon & lat from data
     var apiUrl = "http://api.openweathermap.org/data/2.5/uvi?appid=cbf23c1f1f5aaf3179f3e715be9b2e92&amp;lat=" + latitude + "&amp;lon=" + longitude;
 
@@ -31,7 +33,7 @@ var getUVIndex = function (latitude, longitude, weatherData) {
         // response was successful
         if (response.ok) {
             response.json().then(function (uvData) {
-                displayWeatherData(weatherData, uvData);
+                displayWeatherData(weatherData, uvData, latitude, longitude);
             });
         } else {
             alert("Problem getting UV Index!")
@@ -40,16 +42,69 @@ var getUVIndex = function (latitude, longitude, weatherData) {
 
 };
 
-var displayWeatherData = function (weatherData, uvData) {
+var getForecastData = function (latitude, longitude) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=cbf23c1f1f5aaf3179f3e715be9b2e92";
+    // get forecast data from api
+    fetch(apiUrl).then(function (response) {
+        response.json().then(function (forecastData) {
+            displayForecastData(forecastData);
+        })
+    })
+};
+
+var displayForecastData = function (forecastData) {
+    console.log(forecastData);
+    // loop through forecast array dates
+
+    // set current 'add day' value for date display
+    var d = 1
+    for (var i = 4; i <= 36; i = i + 8) {
+        console.log(forecastData.list[i])
+        var currentForecastDay = forecastData.list[i];
+
+        // create card for current forecasted day
+        var forecastDataEl = document.createElement("card");
+        forecastDataEl.classList = "forecast-card"
+
+        // create h element for forecast date
+        var forecastTitleEl = document.createElement("h4");
+        forecastTitleEl.textContent = moment().add(d, "day").format("MM/DD/YYYY");
+        forecastDataEl.appendChild(forecastTitleEl);
+        // add 1 to d so so that the following day is displayed
+        d++;
+
+        // create p element for forecasted temp
+        var forecastTempEl = document.createElement("p");
+        forecastTempEl.textContent = "Temp: " + currentForecastDay.main.temp;
+        forecastDataEl.appendChild(forecastTempEl);
+
+        // create p element for forecast wind
+        var forecastWindEl = document.createElement("p");
+        forecastWindEl.textContent = "Wind: " + currentForecastDay.wind.speed;
+        forecastDataEl.appendChild(forecastWindEl);
+
+        // create p element for forecast humidity
+        var forecastHumidityEl = document.createElement("p");
+        forecastHumidityEl.textContent = "Humidity: " + currentForecastDay.main.humidity;
+        forecastDataEl.appendChild(forecastHumidityEl);
+
+        forecastWeatherContainer.appendChild(forecastDataEl);
+
+    };
+
+
+}
+
+var displayWeatherData = function (weatherData, uvData, latitude, longitude) {
     console.log(weatherData);
     console.log(weatherData.name);
-    console.log(weatherData.main.temp + " Degrees");
+    console.log(weatherData.main.temp + " ºF");
     console.log(weatherData.wind.speed + " MPH");
     console.log(weatherData.weather[0].icon);
 
     // create current weather card
     var currentWeatherEl = document.createElement("card");
-    currentWeatherEl.classList = "d-flex flex-column border border-dark p-2 m-2 w-100";
+    currentWeatherEl.classList = "d-flex flex-column p-2 m-2 w-100";
 
     // create h element for current weather title
     var cityDateEl = document.createElement("h3")
@@ -58,7 +113,7 @@ var displayWeatherData = function (weatherData, uvData) {
 
     // create p element for current temp
     var tempEl = document.createElement("p");
-    tempEl.textContent = "Temp: " + weatherData.main.temp + " Degrees";
+    tempEl.textContent = "Temp: " + weatherData.main.temp + " ºF";
     currentWeatherEl.appendChild(tempEl);
 
     // create p element for current wind speed
@@ -79,6 +134,7 @@ var displayWeatherData = function (weatherData, uvData) {
     // append currentWeatherEl to parent container on screen
     currentWeatherContainer.appendChild(currentWeatherEl);
 
-}
+    getForecastData(latitude, longitude);
+};
 
 getWeatherData("raleigh");
